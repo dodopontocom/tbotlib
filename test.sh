625e731b8a942319a5e54c42ecbfd978005d150d
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-27 20:27:26 +0000

    adding more options to usage

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-while $1 ; do
+while [ "$1" != ""] ; do
 	case "${1}" in
-		"--message") message=${2}; echo $message; shift 2 ;;
-		"--short") short="on"; shift 1 ;;
-		*) echo ruim ;;
+		"-message") message=${2}; echo $message; shift 2 ;;
+		"-short") short="on"; shift 1 ;;
+#		*) echo ruim; exit -1 ;;
 	esac
 done

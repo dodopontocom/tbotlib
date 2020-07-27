ba4e47f01c1591428b89b0e8c76b7c544eced0a1
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-27 20:28:26 +0000

    adding more options to usage

--- tbotlibs/extras/WelcomeMessage.sh
+++ tbotlibs/extras/WelcomeMessage.sh
@@ -7,7 +7,7 @@ WelcomeMessage.send() {
         case "${1}" in
             "--message") user_message="${2}"; shift 2 ;;
             "--short") short="on"; shift 1 ;;
-            "") WelcomeMessage.usage; exit -1 ;;
+            # "") WelcomeMessage.usage; exit -1 ;;
         esac
     done
 

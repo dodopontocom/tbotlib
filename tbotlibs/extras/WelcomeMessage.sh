3826f8a31167e04a20495143822b4dd762abfe0e
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-27 18:39:00 +0000

    usages

--- tbotlibs/extras/WelcomeMessage.sh
+++ tbotlibs/extras/WelcomeMessage.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
 WelcomeMessage.send() {
-    echo main
+	local message
+
+	message="🆔 [@${message_new_chat_member_username[$id]:-null}]\n"
+    message+="🗣 Olá *${message_new_chat_member_first_name[$id]}*"'!!\n\n'
+    message+="Seja bem-vindo(a) ao *${message_chat_title[$id]}*.\n\n"
+    message+="`$1`"
+
+	ShellBot.sendMessage --chat_id ${message_chat_id[$id]} \
+							--text "$(echo -e $message)" \
+							--parse_mode markdown
+	echo é
+    return 0
 }
 
 WelcomeMessage.usage() {

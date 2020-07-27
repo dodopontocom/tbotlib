f41d679999349d5b72bfe6cd781001933882c7a8
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-27 20:00:14 +0000

    adding more options to usage

--- tbotlibs/extras/WelcomeMessage.sh
+++ tbotlibs/extras/WelcomeMessage.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
 WelcomeMessage.send() {
-	local message
+
+    local message user_message short
+    while [ "${1}" != "" ]; do
+        case "${1}" in
+            "--message") user_message="$2"; shift 2 ;;
+            "--short") short="on"; shift 1 ;;
+        esac
+    done
 
     if [[ ${message_new_chat_member_id[$id]} ]]; then
-        message="🆔 [@${message_new_chat_member_username[$id]:-null}]\n"
-        message+="🗣 Olá *${message_new_chat_member_first_name[$id]}*"'!!\n\n'
-        message+="Seja bem-vindo(a) ao *${message_chat_title[$id]}*.\n\n"
-        message+="\`$1\`"
-
+        if [[ ! ${short} ]]; then
+            message="🆔 [@${message_new_chat_member_username[$id]:-null}]\n"
+            message+="🗣 Olá *${message_new_chat_member_first_name[$id]}*"'!!\n\n'
+            message+="Seja bem-vindo(a) ao *${message_chat_title[$id]}*.\n\n"
+            message+="\`${user_message}\`"
+        else
+            message="\`${user_message}\`"
+        fi
         ShellBot.sendMessage --chat_id ${message_chat_id[$id]} \
                                 --text "$(echo -e $message)" \
                                 --parse_mode markdown
@@ -21,7 +31,9 @@ WelcomeMessage.send() {
 
 WelcomeMessage.usage() {
     echo.PRETTY "WelcomeMessage sends a message to new member of a group"
-    echo.PRETTY "usage: WelcomeMessage.send \"<YOUR WELCOME MESSAGE>\""
+    echo.PRETTY "usage: WelcomeMessage.send --message \"<YOUR WELCOME MESSAGE>\""
+    echo.PRETTY "option: '--message' \"<your welcome message>\" ~> (required)"
+    echo.PRETTY "option: '--short' ~> when you standard message header (optional)"
 
     # local param=$(getopt --name "$FUNCNAME" --options 't:mfsu:l:o:r:d:' \
     #     --longoptions 'token:,

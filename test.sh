012e58c92f8c54cc8b6564d85ad90dfed1f584ae
Rodolfo Neto <rodolfotiago@gmail.com>
2020-07-29 13:52:00 +0000

    testing

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-while [ "$1" != ""] ; do
-	case "${1}" in
-		"-message") message=${2}; echo $message; shift 2 ;;
-		"-short") short="on"; shift 1 ;;
-#		*) echo ruim; exit -1 ;;
-	esac
-done

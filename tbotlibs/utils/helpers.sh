9e9391f64e1a6d48f7cb241666efd740c3a320a2
Rodolfo Neto <rodolfotiago@gmail.com>
2020-08-04 13:21:52 +0000

    ok

--- tbotlibs/utils/helpers.sh
+++ tbotlibs/utils/helpers.sh
@@ -203,5 +203,29 @@ helper.random() {
 }
 
 tbotlib.polling() {
-    
+
+    while true
+    do
+        ShellBot.getUpdates --limit 100 --offset $(ShellBot.OffsetNext) --timeout 30
+
+        for id in $(ShellBot.ListUpdates)
+        do
+            (
+                ShellBot.watchHandle --callback_data ${callback_query_data[$id]}
+
+                [[ ${message_new_chat_member_id[$id]} ]] && WelcomeMessage.send --short --message "I like you"
+                
+                case ${callback_query_data[$id]} in
+                    tick_to_false.${motion_button}) BooleanInlineButton.tick_to_false ${motion_button} ;;
+                    tick_to_true.${motion_button}) BooleanInlineButton.tick_to_true ${motion_button} ;;
+                esac
+                
+                if [[ ${message_entities_type[$id]} == bot_command ]]; then
+                    if [[ "$(echo ${message_text[$id]%%@*} | grep "^\/switch" )" ]]; then
+                        BooleanInlineButton.init --true-value "ON" --false-value "OFF" --button-name "${motion_button}"
+                    fi
+                fi
+            ) &
+        done
+    done
 }
